5f1ade5fce74a11315a00f2f2aa51d1800e927fa
Sp3k8eRX <sp3k8erx@gmail.com>
2019-12-18 21:56:48 +0200

    Done 2,3,4

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const range = null;
+const range = (start, end) => {
+  const arr = [];
+  let j = 0;
+  for (let i = start; i <= end; i++) {
+    arr[j++] = i;
+  }
+  return arr;
+};
+
 
 module.exports = { range };
